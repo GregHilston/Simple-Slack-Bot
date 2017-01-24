e50f227b4c1b798967f4f5f9b2b2ca84a83a8851
GregHilston <Gregory.Hilston@gmail.com>
2017-01-24 08:20:48 -0500

    Working on the ability for mentions to be registered to that supports all users and not just the bot

--- simple_slack_bot/simple_slack_bot.py
+++ simple_slack_bot/simple_slack_bot.py
@@ -1,27 +1,6 @@
 import os, sys, time, logging
 from slackclient import SlackClient
 
-# setting up our logger to write to a log file
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG) # Process all levels of logging messages
-
-# create file handler
-file_handler = logging.FileHandler(__name__  + ".log", mode='w')
-file_handler.setLevel(logging.DEBUG)
-
-# create stream handler
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-stream_handler.setFormatter(formatter)
-
-# add the handlers to the logger
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
-
 
 class SimpleSlackBot():
     """
@@ -31,16 +10,72 @@ class SimpleSlackBot():
     """
 
     def __init__(self):
+        self._logger = self.initialize_logger()
+
         self._BOT_ID = os.environ.get("BOT_ID")
+        if self._BOT_ID is None:
+            self._logger.warning("environment variable BOT_ID is None")
+
         self._SLACK_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+        if self._SLACK_TOKEN is None:
+            self._logger.warning("environment variable SLACK_BOT_TOKEN is None")
+
         self._slack_client = SlackClient(self._SLACK_TOKEN)
         self._hello_callbacks = []
         self._mentions_callbacks = []
         self._public_channels_callbacks = []
         self._private_channels_callbacks = []
         self._direct_messages_callbacks = []
-        logger.info("initialized")
-        logger.info("name of module is {}".format(__name__))
+        self._user_names = []
+        self._user_name_mentions = []
+        self._logger.info("initialized")
+
+
+    def initialize_logger(self):
+        """
+        Initializes and returns a logger
+        """
+
+        # setting up our logger to write to a log file
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG) # Process all levels of logging messages
+
+        # create file handler
+        file_handler = logging.FileHandler(__name__  + ".log", mode='w')
+        file_handler.setLevel(logging.DEBUG)
+
+        # create stream handler
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        stream_handler.setFormatter(formatter)
+
+        # add the handlers to the logger
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+
+        return logger
+
+
+    def update_user_names(self):
+        """
+        Updates this local cache of user names and mention strings from the server
+        """
+
+        AT_USER_PREFIX = "<@"
+        AT_USER_SUFFIX = ">"
+
+        self._user_names = self.get_user_names()
+        self._user_name_mentions = []
+
+        self._user_name_mentions.append(AT_USER_PREFIX + self._BOT_ID + AT_USER_SUFFIX)
+        for user_name in self._user_names:
+            self._user_name_mentions.append(AT_USER_PREFIX + user_name + AT_USER_SUFFIX)
+
+        self._logger.debug("self._user_name_mentions {} of type {}".format(self._user_name_mentions, type(self._user_name_mentions)))
 
 
     def get_slack_client(self):
@@ -50,6 +85,7 @@ class SimpleSlackBot():
         This is useful if you are writing more advanced bot and want complete
         access to all SlackClient has to offer.
         """
+
         return self._slack_client
 
 
@@ -61,12 +97,12 @@ class SimpleSlackBot():
         ret = self._slack_client.rtm_connect()
 
         if ret:
-            logger.info("started!")
+            self._logger.info("started!")
             self.listen()
         else:
-            logger.error("Connection failed. Are you connected to the internet?"
+            self._logger.error("Connection failed. Are you connected to the internet?"
                          " Potentially invalid Slack token or bot ID? Check"
-                         " environment variables.")
+                         " environment variables \"BOT_ID\" and \"SLACK_BOT_TOKEN\"")
 
 
     def route_dictionary_to_notify(self, dictionary):
@@ -74,17 +110,17 @@ class SimpleSlackBot():
         Routes the ductionary to the correct notify
         """
 
-        AT_BOT = "<@" + self._BOT_ID + ">" # Used for mentions
         event_type = dictionary["type"]
-        logger.debug("received an event of type {}".format(event_type))
-        logger.debug("dictionary {}".format(dictionary))
+        self._logger.debug("received an event of type {}".format(event_type))
+        self._logger.debug("dictionary {}".format(dictionary))
+        self.update_user_names()
 
         if event_type == "hello":
             self.notify_hello(dictionary)
 
         elif event_type == "message":
 
-            if AT_BOT in dictionary["text"]:
+            if any(user_name_mention in dictionary["text"] for user_name_mention in self._user_name_mentions):
                 self.notify_mentions(dictionary)
 
             elif dictionary["channel"] in self.get_public_channel_ids():
@@ -97,7 +133,7 @@ class SimpleSlackBot():
                 self.notify_direct_messages(dictionary)
 
             else:
-                logger.error("Unsure how to route {}".format(dictionary))
+                self._logger.error("Unsure how to route {}".format(dictionary))
 
 
     def listen(self):
@@ -108,7 +144,7 @@ class SimpleSlackBot():
         READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
         running = True
 
-        logger.info("began listening!")
+        self._logger.info("began listening!")
 
         while True:
             try:
@@ -121,7 +157,7 @@ class SimpleSlackBot():
 
                 time.sleep(READ_WEBSOCKET_DELAY)
             except KeyboardInterrupt:
-                logger.info("User ended listening. Gracefully shutting down")
+                self._logger.info("User ended listening. Gracefully shutting down")
                 sys.exit(0)
 
 
@@ -132,9 +168,9 @@ class SimpleSlackBot():
 
         if callback not in self._hello_callbacks:
             self._hello_callbacks.append(callback)
-            logger.info("registered {} to hello event".format(str(callback)))
+            self._logger.info("registered {} to hello event".format(str(callback)))
         else:
-            logger.warning("did not register {} to hello event, as already registered".format(str(callback)))
+            self._logger.warning("did not register {} to hello event, as already registered".format(str(callback)))
 
 
     def notify_hello(self, dictionary):
@@ -144,7 +180,7 @@ class SimpleSlackBot():
 
         for callback in self._hello_callbacks:
             callback(dictionary)
-            logger.debug("notified {} of hello event".format(callback))
+            self._logger.debug("notified {} of hello event".format(callback))
 
 
     def register_mentions(self, callback):
@@ -154,9 +190,9 @@ class SimpleSlackBot():
 
         if callback not in self._mentions_callbacks:
             self._mentions_callbacks.append(callback)
-            logger.info("registered {} to mentions".format(str(callback)))
+            self._logger.info("registered {} to mentions".format(str(callback)))
         else:
-            logger.warning("did not register {} to mentions, as already registered".format(str(callback)))
+            self._logger.warning("did not register {} to mentions, as already registered".format(str(callback)))
 
 
     def notify_mentions(self, dictionary):
@@ -166,7 +202,7 @@ class SimpleSlackBot():
 
         for callback in self._mentions_callbacks:
             reply = callback(dictionary)
-            logger.debug("notified {} of mentions event".format(callback))
+            self._logger.debug("notified {} of mentions event".format(callback))
 
             if reply:
                 self.write(dictionary["channel"], reply)
@@ -179,9 +215,9 @@ class SimpleSlackBot():
 
         if callback not in self._public_channels_callbacks:
             self._public_channels_callbacks.append(callback)
-            logger.info("registered {} to public channels".format(str(callback)))
+            self._logger.info("registered {} to public channels".format(str(callback)))
         else:
-            logger.warning("did not register {} to public channels, as already registered".format(str(callback)))
+            self._logger.warning("did not register {} to public channels, as already registered".format(str(callback)))
 
 
     def notify_public_channels_messages(self, dictionary):
@@ -191,7 +227,7 @@ class SimpleSlackBot():
 
         for callback in self._public_channels_callbacks:
             reply = callback(dictionary)
-            logger.debug("notified {} of all public channels event".format(callback))
+            self._logger.debug("notified {} of all public channels event".format(callback))
 
             if reply:
                 self.write(dictionary["channel"], reply)
@@ -204,9 +240,9 @@ class SimpleSlackBot():
 
         if callback not in self._private_channels_callbacks:
             self._private_channels_callbacks.append(callback)
-            logger.info("registered {} to private channels".format(str(callback)))
+            self._logger.info("registered {} to private channels".format(str(callback)))
         else:
-            logger.warning("did not register {} to private channels event, as already registered".format(str(callback)))
+            self._logger.warning("did not register {} to private channels event, as already registered".format(str(callback)))
 
 
     def notify_private_channels_messages(self, dictionary):
@@ -216,7 +252,7 @@ class SimpleSlackBot():
 
         for callback in self._private_channels_callbacks:
             reply = callback(dictionary)
-            logger.debug("notified {} of all private channels event".format(callback))
+            self._logger.debug("notified {} of all private channels event".format(callback))
 
             if reply:
                 self.write(dictionary["channel"], reply)
@@ -228,9 +264,9 @@ class SimpleSlackBot():
 
         if callback not in self._direct_messages_callbacks:
             self._direct_messages_callbacks.append(callback)
-            logger.info("registered {} to direct messages event".format(str(callback)))
+            self._logger.info("registered {} to direct messages event".format(str(callback)))
         else:
-            logger.warning("did not register {} to direct messages event, as already registered".format(str(callback)))
+            self._logger.warning("did not register {} to direct messages event, as already registered".format(str(callback)))
 
 
     def notify_direct_messages(self, dictionary):
@@ -240,7 +276,7 @@ class SimpleSlackBot():
 
         for callback in self._direct_messages_callbacks:
             reply = callback(dictionary)
-            logger.debug("notified {} of all direct messages event".format(callback))
+            self._logger.debug("notified {} of all direct messages event".format(callback))
 
             if reply:
                 self.write(dictionary["user"], reply)
@@ -252,7 +288,7 @@ class SimpleSlackBot():
         """
 
         self._slack_client.api_call("chat.postMessage", channel=channel, text=content, as_user=True)
-        logger.debug("wrote {}".format(content))
+        self._logger.debug("wrote {}".format(content))
 
 
     def get_public_channel_ids(self):
@@ -267,9 +303,9 @@ class SimpleSlackBot():
             public_channel_ids.append(channel["id"])
 
         if len(public_channel_ids) == 0:
-            logger.warning("got no public channel ids")
+            self._logger.warning("got no public channel ids")
         else:
-            logger.debug("got public channel ids {}".format(public_channel_ids))
+            self._logger.debug("got public channel ids {}".format(public_channel_ids))
 
         return public_channel_ids
 
@@ -283,17 +319,16 @@ class SimpleSlackBot():
 
         private_channels = self._slack_client.api_call("groups.list", token=self._SLACK_TOKEN)
 
-        logger.info("private_channels {}".format(private_channels))
+        self._logger.info("private_channels {}".format(private_channels))
         for private_channel in private_channels["groups"]:
             private_channels.append(channel["id"])
 
         if len(private_channel_ids) == 0:
-            logger.warning("got no private channel ids")
+            self._logger.warning("got no private channel ids")
         else:
-            logger.debug("got private channel ids {}".format(private_channel_ids))
+            self._logger.debug("got private channel ids {}".format(private_channel_ids))
 
         return private_channel_ids
-
 
 
     def get_user_ids(self):
@@ -308,11 +343,31 @@ class SimpleSlackBot():
             user_ids.append(user["id"])
 
         if len(user_ids) == 0:
-            logger.warning("got no user ids")
+            self._logger.warning("got no user ids")
         else:
-            logger.debug("got user ids {}".format(user_ids))
+            self._logger.debug("got user ids {}".format(user_ids))
 
         return user_ids
+
+
+    def get_user_names(self):
+        """
+        Gets all user names
+        """
+
+        user_names = []
+
+        users_list = self._slack_client.api_call("users.list", token=self._SLACK_TOKEN)
+        for user in users_list["members"]:
+            user_names.append(user["name"])
+
+        if len(user_names) == 0:
+            self._logger.warning("got no user names")
+        else:
+            self._logger.debug("got user names {}".format(user_names))
+
+        return user_names
+
 
     def get_users_in_channel(self, channel_id):
         """
@@ -328,9 +383,9 @@ class SimpleSlackBot():
                     user_ids.append(user_id)
 
         if len(user_ids) == 0:
-            logger.warning("got no user ids for channel {}".format(channel_id))
+            self._logger.warning("got no user ids for channel {}".format(channel_id))
         else:
-            logger.debug("got user ids {}".format(user_ids))
+            self._logger.debug("got user ids {}".format(user_ids))
 
         return user_ids
 
@@ -344,10 +399,10 @@ class SimpleSlackBot():
 
         for channel in channels_list["channels"]:
             if channel["name"] == name:
-                logger.debug("converted {} to {}".format(channel["name"], channel["id"]))
+                self._logger.debug("converted {} to {}".format(channel["name"], channel["id"]))
                 return channel["id"]
 
-        logger.warning("could not convert channel name {} to an id".format(name))
+        self._logger.warning("could not convert channel name {} to an id".format(name))
 
 
     def user_name_to_user_id(self, name):
@@ -359,10 +414,10 @@ class SimpleSlackBot():
 
         for user in users_list["members"]:
             if user["name"] == name:
-                logger.debug("converted {} to {}".format(name, user["id"]))
+                self._logger.debug("converted {} to {}".format(name, user["id"]))
                 return user["id"]
 
-        logger.warning("could not convert name {} to a user id".format(name))
+        self._logger.warning("could not convert name {} to a user id".format(name))
 
 
     def channel_id_to_channel_name(self, channel_id):
@@ -374,10 +429,10 @@ class SimpleSlackBot():
 
         for channel in channels_list["channels"]:
             if channel["id"] == channel_id:
-                logger.debug("converted {} to {}".format(channel_id, channel["name"]))
+                self._logger.debug("converted {} to {}".format(channel_id, channel["name"]))
                 return channel["name"]
 
-        logger.warning("could not convert channel id {} to a name".format(channel_id))
+        self._logger.warning("could not convert channel id {} to a name".format(channel_id))
 
 
     def user_id_to_user_name(self, user_id):
@@ -389,7 +444,7 @@ class SimpleSlackBot():
 
         for user in users_list["members"]:
             if user["id"] == user_id:
-                logger.debug("converted {} to {}".format(user_id, user["name"]))
+                self._logger.debug("converted {} to {}".format(user_id, user["name"]))
                 return user["name"]
 
-        logger.warning("could not convert user id {} to a name".format(user_id))
+        self._logger.warning("could not convert user id {} to a name".format(user_id))
