1e3ee6302c1df65da7f9d888fc26854df4a725f8
GregHilston <Gregory.Hilston@gmail.com>
2017-06-02 15:02:00 -0600

    Updated __init__.py to include the SlackRequest

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = "simple_slack_bot",
     packages = ["simple_slack_bot"], # this must be the same as the name above
-    version = "0.3.1",
+    version = "0.3.2",
     description = "Simple Slack Bot makes writing your next Slack bot incredibly easy. By factoring out common functionality all Slack Bots require, you can focus on writing your business logic.",
     author = "Greg Hilston",
     author_email = "Gregory.Hilston@gmail.com",
     url = "https://github.com/GregHilston/Simple-Slack-Bot", # use the URL to the github repo
-    download_url = "https://github.com/GregHilston/Simple-Slack-Bot/tarball/v0.3.0-alpha",
+    download_url = "https://github.com/GregHilston/Simple-Slack-Bot/tarball/v0.3.2-alpha",
     keywords = ["slack", "bot", "chat", "simple"], # arbitrary keywords
     classifiers = [],
     install_requires=[
--- simple_slack_bot/__init__.py
+++ simple_slack_bot/__init__.py
@@ -1,1 +1,2 @@
 from .simple_slack_bot import SimpleSlackBot
+from .slack_request import SlackRequest
