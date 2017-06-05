21f4dd632e1bf93fe15e7dfe2168927b8f7fb268
GregHilston <Gregory.Hilston@gmail.com>
2017-06-05 14:58:39 -0600

    Changed setup.py to have a proper short and long description. Along with updated simple_slack_bot.py to hopefully import slack_request successfully

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name = "simple_slack_bot",
     packages = ["simple_slack_bot"], # this must be the same as the name above
     version = "0.3.2",
-    description = "Simple Slack Bot makes writing your next Slack bot incredibly easy. By factoring out common functionality all Slack Bots require, you can focus on writing your business logic.",
+    description = "Simple Slack Bot makes writing your next Slack bot incredibly easy",
+    long_description = "Simple Slack Bot makes writing your next Slack bot incredibly easy. By factoring out common functionality all Slack Bots require, you can focus on writing your business logic by simply registering for Slack Events defined by the Slack API",
     author = "Greg Hilston",
     author_email = "Gregory.Hilston@gmail.com",
     url = "https://github.com/GregHilston/Simple-Slack-Bot", # use the URL to the github repo
