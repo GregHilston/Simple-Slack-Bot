b232564ca880a133aff6fe3da34fc3b45df166fe
Greg.Hilston <Gregory.Hilston@gmail.com>
2020-06-07 11:18:00 -0400

    merging master to this old thread

--- Makefile
+++ Makefile
@@ -3,7 +3,21 @@ PID=app.pid
 
 run:
 	$(PYTHON) app.py
+
 start:
 	$(PYTHON) app.py > /dev/null 2>&1 & echo $$! > $(PID)
+
+package:
+	python3 setup.py sdist bdist_wheel
+
+upload-test-pypi:
+	python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+upload-pypi:
+	python3 -m twine upload dist/*
+
+example:
+	python3 example_component.py
+
 kill:
 	kill $$(cat $(PID))
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="simple_slack_bot",
     packages=["simple_slack_bot"],  # this must be the same as the name above
-    version="1.3.0",
+    version="1.3.2",
     description="Simple Slack Bot makes writing your next Slack bot incredibly easy",
     long_description="Simple Slack Bot makes writing your next Slack bot incredibly easy. By factoring out common functionality all Slack Bots require, you can focus on writing your business logic by simply registering for Slack Events defined by the Slack API",
     author="Greg Hilston",
@@ -13,9 +13,9 @@ setup(
     keywords=["slack", "bot", "chat", "simple"],  # arbitrary keywords
     classifiers=[],
     install_requires=[
-        "slacker",
-        "slacksocket",
+        "slacker==0.9.42",
+        "slacksocket>=0.7,!=0.8,<=0.9",
         "pyyaml",
-        "websocket-client==0.48.0", # required to define as our dependency has a dependency which broke backwards compatibility
+        "websocket-client==0.48", # required to define as our dependency has a dependency which broke backwards compatibility
     ],
 )
