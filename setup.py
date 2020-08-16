7c4476fe79da06be5aa89f9a6580c36d11411384
GregHilston <Gregory.Hilston@gmail.com>
2020-08-16 10:11:52 -0400

    Modify pypi description to use README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="simple_slack_bot",
     packages=["simple_slack_bot"],  # this must be the same as the name above
-    version="1.3.2",
+    version="1.3.3",
     description="Simple Slack Bot makes writing your next Slack bot incredibly easy",
-    long_description="Simple Slack Bot makes writing your next Slack bot incredibly easy. By factoring out common functionality all Slack Bots require, you can focus on writing your business logic by simply registering for Slack Events defined by the Slack API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Greg Hilston",
     author_email="Gregory.Hilston@gmail.com",
     url="https://github.com/GregHilston/Simple-Slack-Bot",  # use the URL to the github repo
