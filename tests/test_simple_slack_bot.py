2f0a5f9491a0b71cf176b11097edcd7c2ef99f83
GregHilston <Gregory.Hilston@gmail.com>
2020-08-19 22:45:58 -0400

    testing that extract_slack_socket_response returns real value and iterable when no exception occurs

--- tests/test_simple_slack_bot.py
+++ tests/test_simple_slack_bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import typing
 
 import pytest
 import slacksocket.errors  # type: ignore
@@ -12,14 +13,14 @@ from simple_slack_bot.simple_slack_bot import SimpleSlackBot, SlackRequest
 class MockIterator:
     def __init__(self):
         self.injectable_yield = None
-        self.raiseable_exception = None
+        self.injectable_exception = None
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.raiseable_exception:
-            raise self.raiseable_exception
+        if self.injectable_exception:
+            raise self.injectable_exception
 
         return self.injectable_yield
 
@@ -111,7 +112,7 @@ def test_listen_stops_listening_when_slack_socket_keyboard_interrupt_exception_o
 ):
     # Given
     mock_iterator = MockIterator()
-    mock_iterator.raiseable_exception = slacksocket.errors.ExitError
+    mock_iterator.injectable_exception = slacksocket.errors.ExitError
 
     mock_slack_socket = MockSlackSocket()
     mock_slack_socket.mock_iterator = mock_iterator
@@ -125,6 +126,28 @@ def test_listen_stops_listening_when_slack_socket_keyboard_interrupt_exception_o
 
     # Then
     assert SimpleSlackBot.KEYBOARD_INTERRUPT_EXCEPTION_LOG_MESSAGE in caplog.text
+
+
+def test_extract_slack_socket_reponse_returns_response_when_no_exception_is_raised():
+    # Given
+    mock_iterator = MockIterator()
+    mock_iterator.injectable_yield = 42
+
+    mock_slack_socket = MockSlackSocket()
+    mock_slack_socket.mock_iterator = mock_iterator
+
+    sut = SimpleSlackBot("mock slack bot token")
+    sut._slackSocket = mock_slack_socket
+
+    expected_first_value = 42
+    expected_second_value_type = typing.Iterable
+
+    # When
+    actual_response = sut.extract_slack_socket_response()
+
+    # Then
+    assert expected_first_value == actual_response[0]
+    assert isinstance(actual_response[1], expected_second_value_type)
 
 
 @pytest.mark.parametrize(
@@ -142,7 +165,7 @@ def test_extract_slack_socket_reponse_returns_none_when_non_exiterror_slack_sock
 ):
     # Given
     mock_iterator = MockIterator()
-    mock_iterator.raiseable_exception = slacksocket_error
+    mock_iterator.injectable_exception = slacksocket_error
 
     mock_slack_socket = MockSlackSocket()
     mock_slack_socket.mock_iterator = mock_iterator
