e3ac9c57e842593e8b267a029f0e2ae6d2ca2a0b
GregHilston <Gregory.Hilston@gmail.com>
2020-08-23 18:15:48 -0400

    Wrote tests for type property both when data is found and when it isn't

--- tests/test_slack_request.py
+++ tests/test_slack_request.py
@@ -24,3 +24,28 @@ def test_slack_request_initializer_stores_python_slackclient_and_slack_event():
     # Then
     assert mock_python_slack_client == sut._python_slackclient
     assert mock_slack_event == sut.slack_event
+
+
+def test_type_returns_type_when_stored():
+    # Given
+    expected_type = "foo"
+    mock_slack_event = {"type": expected_type}
+    sut = SlackRequest(python_slackclient=None, slack_event=mock_slack_event)
+
+    # When
+    actual_type = sut.type
+
+    # Then
+    assert expected_type == actual_type
+
+
+def test_type_returns_none_when_not_found():
+    # Given
+    mock_slack_event = {}
+    sut = SlackRequest(python_slackclient=None, slack_event=mock_slack_event)
+
+    # When
+    actual_type = sut.type
+
+    # Then
+    assert None is actual_type
